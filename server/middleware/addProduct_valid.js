2b5cbe715787a2bb1ca51b2efffd7495a9332200
ahmed-kamal91 <nagmstar91@gmail.com>
2025-03-06 00:21:01 +0200

    on progress version on the product code, verficatoin layer added ,capabiliy need to fix related to admin adding product for another seller [need to reiview the logic]

--- server/middleware/addProduct_valid.js
+++ server/middleware/addProduct_valid.js
@@ -1,11 +1,20 @@
 import fs from 'fs'
 import jwt from "jsonwebtoken"
 import productValidSchema from "../validators/productValidSchema.js";
+import User from '../database/models/user.model.js'; // user model
+
+// for context
+const userModel = User;
 
 /*----------------------------------
 THERE IS SOMETHING NOT HANDLED
 THAT ADMIN CAN ADD/ UPDATE PRODUCT
 -----------------------------------*/
+
+/*
+ [token will contain the id generated from mongodb]
+*/
+
 
 /** 
  * function decypt the token
@@ -20,8 +29,100 @@ const decryptToken = (token, res) =>{
 
     const key = "senu123456789senu123456789senu123456789";
     try   { return jwt.verify(token, key) }
-    catch { return res.json({err:"INVALID TOKEN"}) }
+    catch { return null }
 }
+
+
+/** function:
+ * - verify the user coming in token, whether admin or seller
+ */
+export const verifyUser = (req,res, next) =>{
+
+    // check token exist
+    if(!req.headers.token) 
+        {return res.json({msg :"verifyUser:TOKEN NOT EXIST"})}
+
+    // decrypt user data [token in header]
+    const userData = decryptToken(req.headers.token, res);
+
+    if (userData){
+        // CHECK ROLE
+        if(userData.role == "admin" || userData.role == "seller"){
+
+            // store data for later use
+            req.userData = userData;
+            next(); 
+        }
+        else {return res.json({msg :"verifyUser: UNAUTHORIZED ACCESS"});}
+    }
+    else{
+        return res.json({msg :"verifyUser: INVALID TOKEN"});
+    }
+
+
+}
+
+
+
+const get_ID_Email_BasedRole = async(userRole, req) => {
+
+    // DEBUG
+    console.log("INSIDE GETTING ID EMAIL FUNCTION.....AND ROLE IS", userRole);
+
+    var sellerId,sellerEmail;
+
+    // CHECK ROLE
+    if (userRole == "seller"){
+
+        // EXTRACT SELLER ID + EMAIL
+        sellerId = req.userData.id;
+        sellerEmail = req.userData.email;
+
+    }
+    else if ( userRole == "admin"){
+
+        // GET SELLER ID WHO WANT TO ADD THE PRODUCT FOR  [URL]
+        sellerId = req.params.sellerId;
+
+        // DEBUG
+        console.log("GOTTEN SELLER ID FROM URL =====>", sellerId);
+
+        // SEARCH THE SELLER => GET EMAIL
+        userModel.findById(sellerId)
+        .then( foundSeller =>  {
+
+            //DEBUG
+            console.log("AFTER SEARCHING THE SELLER ======>", foundSeller);
+
+            if(foundSeller){  
+
+                //DEBUG
+                console.log("IF SELLER FOUND.......")
+
+                sellerEmail = foundSeller.email; 
+
+                //DEBUG
+                console.log(`/nSELLER EMAIL ====>`, sellerEmail);
+            }
+            else{ 
+                console.log("COULDN'T FIND THE SELLER");
+                return null 
+            }
+
+        })
+        .catch(() => {
+            console.log("COULDN'T FIND THE USER");
+        })
+
+
+    }
+
+    // DEBUG
+    console.log("FINALLY THE ID , EMAIL ===>", sellerId, sellerEmail);
+
+    return ([sellerId, sellerEmail]);
+}
+
 
 
 /**
@@ -33,14 +134,33 @@ const decryptToken = (token, res) =>{
  * - store image
  * - add local path to the product data
  */
-export const storeImg = (req, res)=>{
-    
-    // DECRYPT TOKEN
-    const data = decryptToken(req.headers.token, res);
+export const storeImg = async(req, res)=>{
 
-    // EXTRACT SELLER ID + ADD ON PATH
-    const sellerId = data.sellerId;
-    const dirPath = `uploads/${sellerId}`;
+    // DEBUG
+    console.log("HELLO FROM INSIDE THE STOREIMAGE");
+
+    const userRole = req.userData.role;
+
+    //DEBUG
+    console.log("THE ROLE ==>", userRole);
+
+    const ID_Email = await get_ID_Email_BasedRole(userRole, req)
+
+
+    // DEBUG
+    console.log("GET ID AND EMAIL BASED ON USER ROLE", ID_Email);
+
+    if(!ID_Email){
+        return res.json({msg: "admin privillages: seller not found, check its id on url"});
+    }
+
+    const [sellerId, sellerEmail] = ID_Email;
+
+    const dirPath = `uploads/${sellerEmail}`;
+
+
+    //DEBUG
+    console.log("DIR PATH ===> ", dirPath);
 
     //-QUICK ADD sellerId to body------
     req.body.sellerId = sellerId;
@@ -55,13 +175,16 @@ export const storeImg = (req, res)=>{
         // IMAGE PATH
         const imgPath = `${dirPath}/${req.file.originalname}`;
 
+
+        //DEBUG
+        console.log("IMAGE PATH ==>", imgPath);
+
         // WRITE FILE TO DISK FROM BUFFER
         fs.writeFileSync(imgPath, req.file.buffer);
 
-        // ADD IMAGE PATH TO `req.body`
+        //--ADD IMAGE PATH TO `req.body`--
         req.body.imagePath = imgPath;
-
-   
+        //--------------------------------
 
 }
 
@@ -74,14 +197,15 @@ export const storeImg = (req, res)=>{
  */
 export const validateImg = (req, res) => {
 
-    console.log("===req.file:==========> ", req.file);
-
     // check image exist
     if(!req.file) {return res.json({err: "image didn't uploaded"})};
 
     // check image size
     const maxSize = 100*1024*1024;
     if(req.file.size >= maxSize){return res.json({err:"image size exceed 100mb"})};
+
+    // DEBUG
+    console.log("HELLO AGAIN BEFORE STORE THE IMAGE....");
 
     // store image
     storeImg(req,res);
@@ -112,6 +236,9 @@ export const validateProduct = (req, res, next) =>{
             errors: validation.error.details.map(err => err.message)
         })
     }
+
+    //debug
+    console.log("HELLO IN IMAGE VALIDATION BEGIN....")
 
     //-validate the image---
     validateImg(req,res);
