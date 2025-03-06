3f556022242c9410b5c0fdfa4a246381c3d419b0
ahmed-kamal91 <nagmstar91@gmail.com>
2025-03-06 04:41:14 +0200

    matching the local branch with remote

--- server/database/models/seller.model.js
+++ server/database/models/seller.model.js
@@ -7,6 +7,9 @@ const SellerSchema = new Schema(
       ref: "User",
       required: true,
     },
+    //--[SENU]: WARNING: <email duplicated> ---> exist in user schema [parent]-----
+    email: { type: String, unique: true, sparse: true },
+    //----------------------------------------------------------------------------
     businessName: { type: String, required: true },
     businessDetails: { type: Object, required: true },
     bankDetails: { type: Object, required: true },
@@ -15,10 +18,24 @@ const SellerSchema = new Schema(
       enum: ["pending", "approved", "rejected"],
       default: "pending",
     },
+    //--[SENU]-:-[LOGIC ERROR]---rating should be array of rating for each customer-------
     ratings: { type: Number, default: 0 },
+    //-----------------------------------------------------------------------------------
+
+    //--[SENU]-:-WARNING:---
+    createdAt: { type: Date, default: Date.now },
     softDelete: { type: Boolean, default: false },
+
+    //--[SENU]-:-[LOGIC ADD]-----to store products related to seller------------------------------
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', default: [] }] 
+    //--------------------END---------------------------------------------------------
+
   },
-  { timestamps: true, versionKey: false }
+
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 const sellerModel = model("Seller", SellerSchema);
