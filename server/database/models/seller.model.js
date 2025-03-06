7269f7bf986b8fd226354e0d24d32b743d33c593
ahmed-kamal91 <nagmstar91@gmail.com>
2025-03-06 03:54:16 +0200

    fixing some issues related to seller schema

--- server/database/models/seller.model.js
+++ server/database/models/seller.model.js
@@ -7,7 +7,9 @@ const SellerSchema = new Schema(
       ref: "User",
       required: true,
     },
+    //--[SENU]: WARNING: <email duplicated> ---> exist in user schema [parent]-----
     email: { type: String, unique: true, sparse: true },
+    //----------------------------------------------------------------------------
     businessName: { type: String, required: true },
     businessDetails: { type: Object, required: true },
     bankDetails: { type: Object, required: true },
@@ -16,10 +18,20 @@ const SellerSchema = new Schema(
       enum: ["pending", "approved", "rejected"],
       default: "pending",
     },
+    //--[SENU]-:-[LOGIC ERROR]---rating should be array of rating for each customer-------
     ratings: { type: Number, default: 0 },
+    //-----------------------------------------------------------------------------------
+
+    //--[SENU]-:-WARNING:---
     createdAt: { type: Date, default: Date.now },
     softDelete: { type: Boolean, default: false },
+
+    //--[SENU]-:-[LOGIC ADD]-----to store products related to seller------------------------------
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', default: [] }] 
+    //--------------------END---------------------------------------------------------
+
   },
+
   {
     timestamps: true,
     versionKey: false,
