fc6f4e17894329aed238e36fb87742d58669da50
ahmed <ahmed@kali-Ahmed.kali-Ahmed>
2025-03-08 01:12:24 +0200

    stagged one

--- server/controllers/seller.controller.js
+++ server/controllers/seller.controller.js
@@ -99,6 +99,6 @@ export const softDeleteSeller = async (req, res) => {
 
     res.status(200).json({ message: "Seller has been soft deleted", seller });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 };
