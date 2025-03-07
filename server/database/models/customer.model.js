9cb7b0d04a9bc8698b4bbef4637580b03e77cfcf
ahmed-kamal91 <nagmstar91@gmail.com>
2025-03-07 16:21:35 +0200

    customer profile not exxist so I create it due to its imediate need

---
+++ server/database/models/customer.model.js
@@ -0,0 +1,49 @@
+import {Schema, model} from "mongoose";
+
+// [SENU] CREATED
+const customerSchema = new mongoose.Schema(
+  {
+    // ID 
+    _id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    // PAYMENT
+    paymentMethods: {
+      type: [String],
+      default: []
+    },
+
+    // WISH LIST [BONUS]
+    wishlist: {
+      type: [Schema.Types.ObjectId],
+      ref: "Product",
+      default: []
+    },
+
+    // LOYALITY POINT [REDUCE PRICE][BONUS]
+    loyaltyPoints: {
+      type: Number,
+      default: 0
+    },
+
+    // DIFFERENT LANG [BONUS]
+    preferredLanguage: {
+      type: String,
+      default: "en"
+    },
+
+    // NOTIFICATION [TIME BOUNDED]
+    notifications: {
+      type: Object,
+      default: {}
+    }
+  },
+  { timestamps: true }
+);
+
+const customerModel = mongoose.model("Customer", customerSchema);
+
+export default customerModel;
