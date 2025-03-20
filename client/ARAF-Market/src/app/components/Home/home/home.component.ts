1e138184c22c6ca538dad999e4c32544a4b47529
ahmed <ahmed@kali-Ahmed.kali-Ahmed>
2025-03-21 01:16:10 +0200

    add tokenInterceptor

--- client/ARAF-Market/src/app/components/Home/home/home.component.ts
+++ client/ARAF-Market/src/app/components/Home/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
-import { TestCorsComponent } from "../../others/test-cors/test-cors.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, TestCorsComponent],
+  imports: [HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
