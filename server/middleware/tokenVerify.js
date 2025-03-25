6b6190469f8b84f39ed9aed8faa0ef65204e84f0
ahmed-kamal91 <nagmstar91@gmail.com>
2025-03-25 04:43:22 +0200

    nothing [just debugs and removed + add commments for clarification]

--- server/middleware/tokenVerify.js
+++ server/middleware/tokenVerify.js
@@ -1,23 +1,25 @@
 import jwt from "jsonwebtoken";
 
 export async function tokenVerify(req, res, next) {
-  console.log("Enter Token Verify");
-
+  
+  // get token from header
   const token = req.headers["token"];
-  console.log(token);
 
-  if (!token) {
-    return res.status(401).json({ message: "Token is required" });
-  }
+  // check existence
+  if (!token) {return res.status(401).json({ message: "Token is required" });}
+
+  // decod the token
   jwt.verify(token, "ARAF", (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "tokenVerify: Invalid token" });
-    }
 
+    // check error occur
+    if (err) {return res.status(401).json({ message: "tokenVerify: Invalid token" });}
+
+    // add decoded in request
     req.user = decoded.user;
     req.user.id = req.user._id;
-    console.log("next ");
-    
+
+    // OK
     next();
+
   });
 }
